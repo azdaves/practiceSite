53292101d146b28897b1edbbaef6aa24ae12986b
“azdaves” <azdaveschilling@yahoo.com>
2018-09-26 10:37:46 -0700

    changes

--- js/random.js
+++ js/random.js
@@ -4,4 +4,6 @@ var input = prompt("Please type a second number");
 var topNumber = parseInt(input);
 var randomNumber = Math.floor( Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;
 var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and " + topNumber + ".</p>";
+// if (isNaN(lower) || isNaN(upper) {
+//     throw new Error(' Please input a number');
 document.write(message);
--- js/randomnumber.js
+++ js/randomnumber.js
@@ -1,8 +1,22 @@
+
+// var lower = prompt('Input a number ');
+// var upper = prompt('Input a higher number ');
 function getRandomNumber ( lower, upper ) {
-  return Math.floor(Math.random() * (upper - lower + 1 )) + lower; 
+  return Math.floor(Math.random() * (upper - lower + 1 )) + lower;
+  if (isNaN(lower) || isNaN(upper) {
+    throw new Error(' Please input a number');
+  }
              
 }
-document.write('<h1> Here is a random ' + getRandomNumber (1, 10) + '</h1>');
-document.write('<h1> Here is a random ' + getRandomNumber (100, 1000) + '</h1>');                              
-document.write('<h1> Here is a random ' + getRandomNumber (5, 50) + '</h1>');
-document.write('<h1> Here is a random ' + getRandomNumber (1000, 10000) + '</h1>');
+document.write('<h1> Here is your random ' + getRandomNumber(lower, upper) + '</h1>');
+
+
+
+
+// var input1 = prompt("Please type a starting number");
+// var bottomNumber = parseInt(input1);
+// var input = prompt("Please type a second number");
+// var topNumber = parseInt(input);
+// var randomNumber = Math.floor( Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;
+// var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and " + topNumber + ".</p>";
+// document.write(message);
